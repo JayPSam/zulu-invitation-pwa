870cc92f343d3c32befc0264b95a5f09ccf917ec
JayPSam <162783308+JayPSam@users.noreply.github.com>
2026-09-07 16:38:12 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,97 @@
 
+const CACHE_NAME = "isimemo-v2";
+
+const FILES = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./manifest.json",
+
+    "./bride.png",
+    "./groom.png",
+
+    "./icon-192.png",
+    "./icon-512.png"
+];
+
+/* =========================
+   INSTALL
+========================= */
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+
+            .then(cache => {
+
+                return cache.addAll(FILES);
+
+            })
+
+    );
+
+    self.skipWaiting();
+
+});
+
+/* =========================
+   FETCH
+========================= */
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+
+            .then(cachedResponse => {
+
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return fetch(event.request);
+
+            })
+
+    );
+
+});
+
+/* =========================
+   ACTIVATE
+========================= */
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys()
+
+            .then(keys => {
+
+                return Promise.all(
+
+                    keys
+
+                        .filter(
+                            key => key !== CACHE_NAME
+                        )
+
+                        .map(
+                            key => caches.delete(key)
+                        )
+                  )
+
+                );
+
+            })
+
+    );
+
+    self.clients.claim();
+
+});
