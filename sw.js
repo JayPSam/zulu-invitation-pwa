f895c7f6a9ed2d5d158e6a40b6d3d98ba037a0ce
JayPSam <162783308+JayPSam@users.noreply.github.com>
2026-09-07 09:28:19 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,56 @@
 
+const CACHE_NAME = "isimemo-v1";
+
+const FILES = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES))
+
+    );
+
+});
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+            .then(cachedResponse => {
+
+                return cachedResponse || fetch(event.request);
+
+            })
+
+    );
+
+});
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(keys => {
+
+            return Promise.all(
+
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+
+            );
+
+        })
+
+    );
+
+});
